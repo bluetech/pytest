c87d986cdd2f1273c54d69121d1ebe8c2c4ea600
Ran Benita <ran@unusedvar.com>
2024-03-09 09:08:44 +0200

    WIP: python: fix instance handling in static and class method tests
    TODO: Clear `_instance` when `_obj` is cleared.
    
    and also fixes a regression in pytest 8.0.0 where `setup_method` crashes
    if the class has static or class method tests.
    
    It is allowed to have a test class with static/class methods which
    request non-static/class method fixtures (including `setup_method`
    xunit-fixture). I take it as a given that we need to support this
    somewhat odd scenario (stdlib unittest also supports it).
    
    This raises a question -- when a staticmethod test requests a bound
    fixture, what is that fixture's `self`?
    
    stdlib unittest says - a fresh instance for the test.
    
    Previously, pytest said - some instance that is shared by all
    static/class methods. This is definitely broken since it breaks test
    isolation.
    
    Change pytest to behave like stdlib unittest here.
    
    In practice, this means stopping to rely on `self.obj.__self__` to get
    to the instance from the test function's binding. This doesn't work
    because staticmethods are not bound to anything.
    
    Instead, keep the instance explicitly and use that.
    
    BTW, I think this will allow us to change `Class`'s fixture collection
    (`parsefactories`) to happen on the class itself instead of a class
    instance, allowing us to avoid one class instantiation. But needs more
    work.
    
    Fixes #12065.

--- src/_pytest/python.py
+++ src/_pytest/python.py
@@ -302,10 +302,10 @@ class PyobjMixin(nodes.Node):
         """Python instance object the function is bound to.
 
         Returns None if not a test method, e.g. for a standalone test function,
-        a staticmethod, a class or a module.
+        a class or a module.
         """
-        node = self.getparent(Function)
-        return getattr(node.obj, "__self__", None) if node is not None else None
+        # Overridden by Function.
+        return None
 
     @property
     def obj(self):
@@ -1752,12 +1752,31 @@ class Function(PyobjMixin, nodes.Item):
         """Underlying python 'function' object."""
         return getimfunc(self.obj)
 
-    def _getobj(self):
-        assert self.parent is not None
+    @property
+    def instance(self):
+        try:
+            return self._instance
+        except AttributeError:
+            if isinstance(self.parent, Class):
+                # Each Function gets a fresh class instance.
+                self._instance: Optional[Any] = self._getinstance()
+            else:
+                self._instance = None
+        return self._instance
+
+    def _getinstance(self):
         if isinstance(self.parent, Class):
             # Each Function gets a fresh class instance.
-            parent_obj = self.parent.newinstance()
+            return self.parent.newinstance()
         else:
+            return None
+
+    def _getobj(self):
+        instance = self.instance
+        if instance is not None:
+            parent_obj = instance
+        else:
+            assert self.parent is not None
             parent_obj = self.parent.obj  # type: ignore[attr-defined]
         return getattr(parent_obj, self.originalname)
 
